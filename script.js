6078fd64e0ab583289bd1f1d29d30ccbf08fe618
Badra Mohamed <moha1150@umn.edu>
2022-05-09 11:35:06 -0500

    ok

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log('Hello scrip.js');
+
+let MyName= 'badra';
+console.log('my name is', MyName);
+
+//console.log('my name is, Badra);
+// let -- a keyword, we are about to declare a variable 
+
+let LastName= 'Mohamed';
+console.log('last name is', LastName);
